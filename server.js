07728d0b2a8cbdb9eee4313a90b7f490c2d562c1
babyo77 <onlybabyyo7@gmail.com>
2024-07-14 11:52:24 +0530

    new-insta-scraper

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const app = require("express")();
 const cors = require("cors");
-const getLinks = require("./utils/igdownloader");
-const port = process.env.PORT || 4000;
+const instagram = require("./utils/igdownloader");
+const port = process.env.PORT || 5000;
 app.use(cors());
 
 app.get("/api/v1", async (req, res) => {
   const url = req.query.link;
   try {
-    const dataList = await getLinks(url);
+    const dataList = await instagram(url);
     res.status(200).json(dataList);
   } catch (error) {
     res.status(403).json(error.message);
